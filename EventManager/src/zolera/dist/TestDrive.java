a1d696cb344c2b533af9c13a9526b95a084779b1
Aldo Amezcua <aldo@zolera.net>
2015-03-02 21:56:10 -0600

    Adding random events

--- EventManager/src/zolera/dist/TestDrive.java
+++ EventManager/src/zolera/dist/TestDrive.java
@@ -1,27 +1,37 @@
 package zolera.dist;
 
 import java.util.Scanner;
+import java.util.TimerTask;
+
 import zolera.dist.EventManager.Event;
 
-public class TestDrive {
+public class TestDrive extends TimerTask {
 	public static int[] hosts = {1521, 1522, 1523, 1524};
-	public static Event [] randomEvents = {	Event.SENDEVENT, 
-											Event.SENDEVENT,
-											Event.SENDEVENT,
-											Event.SENDEVENT,
-											Event.SENDEVENT,
-											Event.SENDEVENT,
-											Event.SENDEVENT,
-											Event.SENDEVENT,
-											Event.SENDEVENT,
-											Event.SENDEVENT,
-											Event.SENDEVENT
+	public static Event [] randomEvents = {	Event.ACKNOWLEDGE_CS, 
+											Event.EXIT,
+											Event.JOIN,
+											Event.PLAY,
+											Event.RELEASE_CS,
+											Event.REQUEST_CS,
+											Event.REQUEST_MASTERSHIP,
+											Event.STOP,
+											Event.TRANSFER_MASTERSHIP,
+											Event.VOLUME,
+											Event.EVENT_1,
+											Event.EVENT_2,
+											Event.EVENT_3
 											};
 	
 	public static void main(String [] args)
 	{
 		Scanner scan = new Scanner(System.in);
 		String whoami = scan.next();
+	}
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		
 	}
 	
 	//public 
